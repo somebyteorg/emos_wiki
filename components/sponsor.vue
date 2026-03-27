67c23310e38dd59cca42d726f6e08d2263640d3a
puzzle <happypuzzle@126.com>
2026-03-27 10:11:36 +0800

    add sponsor

--- components/sponsor.vue
+++ components/sponsor.vue
@@ -570,6 +570,12 @@
       price: 100,
       remark: '私聊',
     },
+    {
+      time: '2026/03/27',
+      name: 'ruoai',
+      price: 200,
+      remark: '私聊',
+    },
   ].reverse()
 
   const goods: Array<{
